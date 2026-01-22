de4544f2109655f94209150e6d506c54450631fb
Vitor Mendes Camilo <vitormendescamilo@usp.br>
2026-01-21 22:52:19 -0300

    Created tcl script to build OLO library in Vivado

---
+++ Tcl/create_libraries_vivado.tcl
@@ -0,0 +1,13 @@
+# This script creates the libraries needed by the RTL in Vivado.
+
+# Get the third party directory
+set script_dir [file dirname [file normalize [info script]]]
+set third_party_dir [file normalize [file join $script_dir ".." ThirdParty]]
+
+# Get the Open Logic base directory
+set olo_dir  [file join $third_party_dir open-logic src base vhdl]
+
+# Grab the files and build the libraries
+set olo_files [glob -nocomplain -directory $olo_dir *.vhd]
+add_files -fileset sources_1 $olo_files
+set_property library openlogic_base [get_files $olo_files]
