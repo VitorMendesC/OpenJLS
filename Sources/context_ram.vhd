a469e2834314e7b99ca91093688db3b4eb680eac
Vitor Mendes Camilo <vitormendescamilo@usp.br>
2026-04-22 16:04:23 -0300

    `context_ram`: Added context values initialization

--- Sources/context_ram.vhd
+++ Sources/context_ram.vhd
@@ -24,30 +24,58 @@ use IEEE.NUMERIC_STD.all;
 library openlogic_base;
 use openlogic_base.olo_base_pkg_math.all;
 
+use work.Common.all;
+
 entity context_ram is
   generic (
-    RAM_DEPTH  : positive := 1024;
-    WORD_WIDTH : positive := 32
+    RANGE_      : positive := CO_RANGE_STD;
+    RAM_DEPTH   : positive := 367;
+    A_WIDTH     : positive := CO_AQ_WIDTH_STD;
+    B_WIDTH     : positive := CO_BQ_WIDTH_STD;
+    C_WIDTH     : positive := CO_CQ_WIDTH;
+    N_WIDTH     : positive := CO_NQ_WIDTH_STD;
+    NN_WIDTH    : positive := CO_NNQ_WIDTH_STD;
+    TOTAL_WIDTH : positive := CO_TOTAL_WIDTH_STD
   );
 
   port (
     iClk    : in std_logic;
+    iRst    : in std_logic;
     iWrAddr : in std_logic_vector(log2ceil(RAM_DEPTH) - 1 downto 0);
     iWrEn   : in std_logic;
-    iWrData : in std_logic_vector(WORD_WIDTH - 1 downto 0);
+    iWrData : in std_logic_vector(TOTAL_WIDTH - 1 downto 0);
     iRdAddr : in std_logic_vector(log2ceil(RAM_DEPTH) - 1 downto 0);
     iRdEn   : in std_logic;
-    oRdData : out std_logic_vector(WORD_WIDTH - 1 downto 0)
+    oRdData : out std_logic_vector(TOTAL_WIDTH - 1 downto 0)
   );
 end context_ram;
 
 architecture Behavioral of context_ram is
+
+  constant A_INIT : natural := math_max(2, (RANGE_ + 32) / 64);
+  constant B_INIT : integer := 0;
+  constant C_INIT : integer := 0;
+  constant N_INIT : natural := 1;
+
+  -- Packed init word (A | B | C | N)
+  -- For run context (A | Nn | 0 | N)
+  -- Nn also initializes to 0, like B
+  constant CTX_INIT : std_logic_vector(TOTAL_WIDTH - 1 downto 0) :=
+  std_logic_vector(to_unsigned(A_INIT, A_WIDTH)) &
+  std_logic_vector(to_signed(B_INIT, B_WIDTH)) &
+  std_logic_vector(to_signed(C_INIT, C_WIDTH)) &
+  std_logic_vector(to_unsigned(N_INIT, N_WIDTH));
+
+  signal sUseInitValue : std_logic_vector(RAM_DEPTH - 1 downto 0) := (others => '1'); -- Indicates if init value should be used for each address
+  signal sUseInitReg   : std_logic                                := '0'; -- Registered flag aligning with BRAM RdLatency=1.
+  signal sBramRdData   : std_logic_vector(TOTAL_WIDTH - 1 downto 0);
+
 begin
 
   olo_base_ram_sdp_inst : entity openlogic_base.olo_base_ram_sdp
     generic map(
       Depth_g         => RAM_DEPTH,
-      Width_g         => WORD_WIDTH,
+      Width_g         => TOTAL_WIDTH,
       IsAsync_g       => false,
       RdLatency_g     => 1,
       RamStyle_g      => "auto",
@@ -66,7 +94,23 @@ begin
       Rd_Clk  => open,
       Rd_Addr => iRdAddr,
       Rd_Ena  => iRdEn,
-      Rd_Data => oRdData
+      Rd_Data => sBramRdData
     );
+
+  process (iClk)
+  begin
+    if rising_edge(iClk) then
+      sUseInitReg <= sUseInitValue(to_integer(unsigned(iRdAddr)));
+
+      if iRst = '1' then
+        sUseInitValue <= (others => '1');
+      elsif iRdEn = '1' then
+        sUseInitValue(to_integer(unsigned(iRdAddr))) <= '0';
+      end if;
+    end if;
+  end process;
+
+  oRdData <= CTX_INIT when sUseInitReg = '1' else
+    sBramRdData;
 
 end Behavioral;
