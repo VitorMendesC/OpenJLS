d6d1dab155e377f6d202782467b3cc4e05e2ae13
Vitor Mendes Camilo <vitormendescamilo@usp.br>
2026-04-22 14:30:01 -0300

    `A20`: Simplified interface and computation

--- Sources/A20_compute_temp.vhd
+++ Sources/A20_compute_temp.vhd
@@ -1,22 +1,13 @@
 ----------------------------------------------------------------------------------
--- Company:
 -- Engineer:    Vitor Mendes Camilo
--- 
--- Create Date: 02/07/2026
--- Design Name: 
+--
 -- Module Name: A20_compute_temp - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:                 Code segment A.20
---                                      Computation of the auxiliary variable TEMP
--- 
+-- Description: Code segment A.20 — computation of the auxiliary variable TEMP.
+--              Q = RItype + 365, so a single context read (Aq, Nq at Q)
+--              supplies everything needed:
+--                RItype = 0 → TEMP = Aq                   (N unused)
+--                RItype = 1 → TEMP = Aq + (Nq >> 1)
+--
 ----------------------------------------------------------------------------------
 
 use work.Common.all;
@@ -32,9 +23,8 @@ entity A20_compute_temp is
   );
   port (
     iRItype : in std_logic;
-    iA365   : in unsigned (A_WIDTH - 1 downto 0);
-    iA366   : in unsigned (A_WIDTH - 1 downto 0);
-    iN366   : in unsigned (N_WIDTH - 1 downto 0);
+    iAq     : in unsigned (A_WIDTH - 1 downto 0);
+    iNq     : in unsigned (N_WIDTH - 1 downto 0);
     oTemp   : out unsigned (A_WIDTH - 1 downto 0)
   );
 end A20_compute_temp;
@@ -42,16 +32,8 @@ end A20_compute_temp;
 architecture Behavioral of A20_compute_temp is
 begin
 
-  process (iRItype, iA365, iA366, iN366)
-    variable vTemp : unsigned (A_WIDTH - 1 downto 0);
-  begin
-    if iRItype = '0' then
-      vTemp := iA365;
+  oTemp <= iAq when iRItype = '0'
     else
-      vTemp := iA366 + resize(shift_right(iN366, 1), vTemp'length);
-    end if;
-
-    oTemp <= vTemp;
-  end process;
+    iAq + resize(shift_right(iNq, 1), A_WIDTH); -- NOTE: Assumes Nq width < Aq width
 
 end Behavioral;
