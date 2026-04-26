05b6ab0463701d255c3b89dc12f0ce8b91f416b5
Vitor Mendes Camilo <vitormendescamilo@usp.br>
2026-04-26 17:14:43 -0300

    `A22`: Renaming

--- Sources/A22_errval_mapping.vhd
+++ Sources/A22_errval_mapping.vhd
@@ -27,11 +27,11 @@ use IEEE.NUMERIC_STD.all;
 
 entity A22_errval_mapping is
   generic (
-    ERR_WIDTH           : natural := CO_ERROR_VALUE_WIDTH_STD;
+    ERROR_WIDTH         : natural := CO_ERROR_VALUE_WIDTH_STD;
     MAPPED_ERRVAL_WIDTH : natural := CO_MAPPED_ERROR_VAL_WIDTH_STD
   );
   port (
-    iErrval   : in signed (ERR_WIDTH - 1 downto 0);
+    iErrval   : in signed (ERROR_WIDTH - 1 downto 0);
     iRItype   : in std_logic;
     iMap      : in std_logic;
     oEMErrval : out unsigned (MAPPED_ERRVAL_WIDTH - 1 downto 0)
