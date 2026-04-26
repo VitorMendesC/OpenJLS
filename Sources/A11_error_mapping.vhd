a3761a6fe582886cec3d6917f0301ae3464ad462
Vitor Mendes Camilo <vitormendescamilo@usp.br>
2026-04-26 17:12:30 -0300

    `A11`: Made it lossless only, generics more explicit and refactor Moved assert to top level;

--- Sources/A11_error_mapping.vhd
+++ Sources/A11_error_mapping.vhd
@@ -17,7 +17,6 @@
 -- Additional Comments:             Code segment A.11
 --                                  Error mapping to non-negative values
 --
--- NOTE: if N_WIDTH > B_WIDTH the code is wrong and will overflow
 ----------------------------------------------------------------------------------
 use work.Common.all;
 
@@ -30,15 +29,14 @@ entity A11_error_mapping is
     N_WIDTH                : natural := CO_NQ_WIDTH_STD;
     B_WIDTH                : natural := CO_BQ_WIDTH_STD;
     K_WIDTH                : natural := CO_K_WIDTH_STD;
-    ERROR_VALUE_WIDTH      : natural := CO_ERROR_VALUE_WIDTH_STD;
-    MAPPED_ERROR_VAL_WIDTH : natural := CO_MAPPED_ERROR_VAL_WIDTH_STD;
-    NEAR                   : natural := CO_NEAR_STD
+    ERROR_WIDTH            : natural := CO_ERROR_VALUE_WIDTH_STD;
+    MAPPED_ERROR_VAL_WIDTH : natural := CO_MAPPED_ERROR_VAL_WIDTH_STD
   );
   port (
     iK              : in unsigned (K_WIDTH - 1 downto 0);
     iBq             : in signed (B_WIDTH - 1 downto 0);
     iNq             : in unsigned (N_WIDTH - 1 downto 0);
-    iErrorVal       : in signed (ERROR_VALUE_WIDTH - 1 downto 0);
+    iErrorVal       : in signed (ERROR_WIDTH - 1 downto 0);
     oMappedErrorVal : out unsigned (MAPPED_ERROR_VAL_WIDTH - 1 downto 0)
   );
 end A11_error_mapping;
@@ -62,16 +60,12 @@ architecture Behavioral of A11_error_mapping is
 
 begin
 
-  assert B_WIDTH >= N_WIDTH
-  report "A11_error_mapping: B_WIDTH must be >= N_WIDTH, else it may overflow!"
-    severity failure;
-
   sErrorValExt <= resize(iErrorVal, sErrorValExt'length);
   -- Extend and compare: 2*B <= -N with one extra bit to prevent overflow
   sBExt <= resize(iBq, sBExt'length);
   sNExt <= resize(signed(iNq), sBExt'length);
 
-  sSpecialMap <= '1' when (NEAR = 0 and iK = 0 and 2 * sBExt <= - sNExt) else
+  sSpecialMap <= '1' when (iK = 0 and 2 * sBExt <= - sNExt) else
     '0';
 
   -- Error sign flag
